492ce78fe51c468698957c359814b8772952def0
charan <sricharanlakkam66@gmail.com>
2021-11-18 20:55:04 +0530

    commit first

--- seatbooking/src/main/java/com/seatbooking/seatbooking/service/UserServiceImpl.java
+++ seatbooking/src/main/java/com/seatbooking/seatbooking/service/UserServiceImpl.java
@@ -1,9 +1,6 @@
 package com.seatbooking.seatbooking.service;
 
-import java.util.List;
 import java.util.Optional;
-
-import javax.swing.text.html.Option;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
